9a4417e41b047a9f982bc27de6aa6e16c1d6600c
GitUser <gituser@gmail.com>
2024-01-13 22:17:50 +0000

    Entry for Relational Tables and made some properations

---
+++ QueryListForRelationalTBLPrep.sql
@@ -0,0 +1,27 @@
+--RELATIONAL TABLE SETTINGS
+
+--ALTER TABLE TBLEXAMS
+--ADD StudentId smallint
+
+--ALTER TABLE TBLEXAMS
+--ADD LessonId smallint
+
+--CREATE TABLE TblCities
+--(
+--Id Tinyint identity(1,1),
+--City varchar(25)
+--)
+
+--INSERT INTO TBLCITIES (CITY) VALUES ('Edinburgh')
+
+--UPDATE TblStudents SET BirthCity=6 WHERE BirthCity='Edinburgh'
+
+--CREATE TABLE TblClubs
+--(
+--Id Tinyint identity(1,1),
+--Club varchar(25)
+--)
+
+--UPDATE TblStudents SET Club=4 WHERE Club='Tennis'
+
+
